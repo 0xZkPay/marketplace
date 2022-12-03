f2e1df0bc31a9650ae884e6d4d574618874702a0
ss251 <ss251@uw.edu>
2022-12-03 18:13:19 +0530

    store zkpay api key in local storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
 import "../styles/globals.css";
 
 function App({ Component, pageProps }) {
+  const API_URL = "http://localhost:9081";
+
+  const [apiKey, setApiKey] = useState("");
+
+  useEffect(() => {
+    if (
+      localStorage.getItem("api_key") === null ||
+      localStorage.getItem("api_key") === undefined
+    ) {
+      fetch(API_URL + "/getKey", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          s,
+        },
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          localStorage.setItem("api_key", json.Key._id);
+        });
+    }
+  }, [apiKey]);
+
   return (
     <div className="bg-black h-screen">
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 export default function Home() {
+  const API_URL = "http://localhost:9081";
+
+  const buyItem = () => {
+    fetch(API_URL + "");
+  };
+
   return (
     <div className="bg-black flex flex-col items-center">
       <div
@@ -12,13 +18,34 @@ export default function Home() {
       <a href="http://localhost:3000" target="_blank" rel="noreferrer">
         <button
           className="hover:bg-gradient-to-r
-            from-indigo-500 via-purple-500 to-indigo-500
+            from-indigo-500 via-purple-500 to-indigo-500 mb-20
             animate-text bg-gradient-to-r from gray-500 via gray-400 to-gray-300 hover:text-indigo-500
             from-indigo-500 hover:text-white via-purple-500 to-indigo-500 border-indigo-500 border-2 rounded-lg p-4 shadow-lg font-bold"
         >
           View Dashboard
         </button>
       </a>
+
+      <div className="border shadow rounded-xl overflow-hidden mb-20">
+        <img style={{ height: "20rem", width: "100%" }} src={"/hat.png"} />
+        <div className="p-4">
+          <p style={{ height: "56px" }} className="text-2xl font-semibold">
+            Hat
+          </p>
+          <div style={{ height: "70px", overflow: "hidden" }}>
+            <p className="text-gray-400">{"Hat"}</p>
+          </div>
+        </div>
+        <div className="p-4 bg-black">
+          <p className="text-2xl mb-2 font-bold text-white">1 BOB</p>
+          <button
+            className="w-full bg-indigo-500 text-white font-bol py-2 px-12 rounded"
+            onClick={() => buyItem()}
+          >
+            Buy
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
